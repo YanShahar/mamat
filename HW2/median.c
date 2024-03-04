0f57403dab8b781171d3b2d12be45c2bb22cc0f3
lavieled <158300455+lavieled@users.noreply.github.com>
2024-03-04 23:23:51 +0200

    Update median.c

--- HW2/median.c
+++ HW2/median.c
@@ -1,20 +1,18 @@
-// 319046504 laviel@campus.technion.ac.il lavie lederman
-// 206159527 shahary@campus.technion.ac.il shahar yankelovich
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//define grade range
+
+// Define grade range
 #define MAX_GRADE 100
 #define MIN_GRADE 0
 
 static FILE *f;
 
-int median(char *argv, int argc);
+int median(char *argv, int size);
 int compare(const void *a, const void *b);
 
 int main(int argc, char **argv) {
-    //decide input method
+    // Decide input method
     if (argc == 1 || !strcmp("-", argv[1])) {
         f = stdin;
     } else {
@@ -27,63 +25,42 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    int result = median(argv[1], argc);
-    printf("%d\n", result);//print median
+    int result = median(argv[1], argc - 2); // Pass the correct size to median function
+    printf("%d\n", result); // Print median
 
     fclose(f);
     return 0;
 }
 
-int median(char *argv, int argc) {
-    int *arr = (int *)malloc(argc * sizeof(int));//init array to sort grades
+int median(char *argv, int size) {
+    int *arr = (int *)calloc(size, sizeof(int)); // Use calloc to initialize to zero
     int grade;
     int retval;
     int line = 0;
 
-   /* for(int i=0;i < argc; ++i){
+    while (line < size) {
         retval = fscanf(f, "%d", &grade);
-        if (retval != 1) { // Error 
+        if (retval != 1) { // Error
             fprintf(stderr, "Error: not a number\n");
             free(arr);
             exit(1);
-        }
-        else if (grade < MIN_GRADE || grade > MAX_GRADE) {//check in range
+        } else if (grade < MIN_GRADE || grade > MAX_GRADE) { // Check in range
             fprintf(stderr, "Error at line %d: grade %d invalid\n", (line + 1), grade);
             free(arr);
             exit(1);
-        }
-        else {
-            arr[line] = grade;//add grade to array
-            line++;
-            //printf("%d\n",grade);
-        }
-    } */
-    while (line <= argc) {
-        retval = fscanf(f, "%d", &grade);
-        if (retval != 1) { // Error 
-            fprintf(stderr, "Error: not a number\n");
-            free(arr);
-            exit(1);
-        }
-        else if (grade < MIN_GRADE || grade > MAX_GRADE) {//check in range
-            fprintf(stderr, "Error at line %d: grade %d invalid\n", (line + 1), grade);
-            free(arr);
-            exit(1);
-        }
-        else {
-            arr[line] = grade;//add grade to array
+        } else {
+            arr[line] = grade; // Add grade to array
             line++;
         }
-        
-    } 
+    }
 
     int median;
-    qsort(arr, line, sizeof(int), compare);//sort array to easily find median
-    median = arr[(line) / 2];//choose median from sorted array
+    qsort(arr, size, sizeof(int), compare); // Sort array to easily find median
+    median = arr[size / 2]; // Choose median from the sorted array
     free(arr);
     return median;
 }
 
 int compare(const void *a, const void *b) {
     return (*(int *)a - *(int *)b);
-}//cmp func for qsort
+} // Compare function for qsort
